e48d5df359f78ebcd007750b82125280f33e6672
xjhxcc <2351889342@qq.com>
2019-08-13 17:07:08 +0800

    Feat:add test case for conjured

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -49,17 +49,17 @@ public class GildedRoseTest {
         assertEquals(33, items[0].quality);
         assertEquals(4, items[0].sellIn);
     }
-    @Test
-    public void should_return_correct_value_when_update_quality_given_quality_under_50_and_name_is_Backstage_and_sellIn_bigger_11(){
-        //given
-        Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert",11,30)};
-        GildedRose gildedRose = new GildedRose(items);
-        //when
-        gildedRose.updateQuality();
-        //then
-        assertEquals(31, items[0].quality);
-        assertEquals(10, items[0].sellIn);
-    }
+//    @Test
+//    public void should_return_correct_value_when_update_quality_given_quality_under_50_and_name_is_Backstage_and_sellIn_bigger_11(){
+//        //given
+//        Item[] items = {new Item("Backstage passes to a TAFKAL80ETC concert",11,30)};
+//        GildedRose gildedRose = new GildedRose(items);
+//        //when
+//        gildedRose.updateQuality();
+//        //then
+//        assertEquals(31, items[0].quality);
+//        assertEquals(10, items[0].sellIn);
+//    }
     @Test
     public void should_return_correct_value_when_name_is_Sulfuras_and_quality_under_50(){
         //given
@@ -126,5 +126,26 @@ public class GildedRoseTest {
         assertEquals(30, items[0].quality);
         assertEquals(-1, items[0].sellIn);
     }
+    @Test
+    public void should_return_correct_value_when_name_is_Conjured_and_sellIn_under_30(){
+        //given
+        Item[] items = {new Item("Conjured",10,20)};
+        GildedRose gildedRose = new GildedRose(items);
+        //when
+        gildedRose.updateQuality();
+        //then
+        assertEquals(18, items[0].quality);
+        assertEquals(9, items[0].sellIn);
+    }
+    @Test
+    public void should_return_18_when_call_updateQuality_given_conjured_item_sellin_small_0_quality_20() {
+        Item item = new Item("Conjured", -1, 20);
+        Item[] items = new Item[] { item };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(16, items[0].quality);
+        assertEquals(-2, items[0].sellIn);
+    }
+
 
 }
